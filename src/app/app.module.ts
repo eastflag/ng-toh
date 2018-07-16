1a903a7f86adcc0a39a35190b4eb5b3a7ee86720
eastflag <eastflag@gmail.com>
2018-07-16 21:23:51 +0900

    custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
 import { JqueryComponent } from './jquery/jquery.component';
 import { MydatePipe } from './mydate.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -38,6 +39,7 @@ const routes: Routes = [
     HomeComponent,
     JqueryComponent,
     MydatePipe,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+  @Input() highlightColor: string;
+
+  constructor(private el: ElementRef) {
+    // el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = null;
+  }
+}
