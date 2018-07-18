060b95c4af763941d06147a97ce41ad6579e9e0a
eastflag <eastflag@gmail.com>
2018-07-18 19:08:09 +0900

    hero 등록 - 유효성 로직 추가

--- src/app/admin/register-hero/register-hero.component.ts
+++ src/app/admin/register-hero/register-hero.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CustomValidators} from 'ng2-validation';
 
 @Component({
   selector: 'app-register-hero',
@@ -12,11 +13,11 @@ export class RegisterHeroComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      name: [null, Validators.compose([Validators.required])],
-      email: [null, Validators.compose([Validators.required])],
-      sex: null,
-      country: [null, Validators.compose([Validators.required])],
-      address: [null, Validators.compose([Validators.required])],
+      name: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(20)])],
+      email: [null, Validators.compose([Validators.required, Validators.email])],
+      sex: [null, Validators.required],
+      country: [null, Validators.required],
+      address: null,
       power: this.fb.array(this.powers.map(x => !1))
     });
   }
@@ -25,6 +26,16 @@ export class RegisterHeroComponent implements OnInit {
   }
 
   register() {
+    console.log('register');
+    if (!this.form.valid) {
+      // to validate all form fields
+      Object.keys(this.form.controls).forEach(key => {
+        const control = this.form.controls[key];
+        control.markAsTouched({onlySelf: true});
+      });
+      return;
+    }
+
     const power = this.form.controls['power'].value.map((item, index) => {
       if (item) {
         return this.powers[index];
