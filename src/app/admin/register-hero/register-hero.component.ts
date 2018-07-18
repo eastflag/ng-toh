2d94333df147f9903e5961c89d1d82a3e549ecb1
eastflag <eastflag@gmail.com>
2018-07-18 21:38:15 +0900

    .

--- src/app/admin/register-hero/register-hero.component.ts
+++ src/app/admin/register-hero/register-hero.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CustomValidators} from 'ng2-validation';
 
 @Component({
   selector: 'app-register-hero',
