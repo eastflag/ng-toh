7b884c06fecdf94caf44e98a6b4b2f7a8c41dc7e
eastflag <eastflag@gmail.com>
2018-07-20 14:45:27 +0900

    hero 관리 - 목록 보기

--- src/app/admin/manage-hero/manage-hero.component.ts
+++ src/app/admin/manage-hero/manage-hero.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from '../../hero.service';
+import {Hero} from '../../hero';
 
 @Component({
   selector: 'app-manage-hero',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-hero.component.scss']
 })
 export class ManageHeroComponent implements OnInit {
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes()
+      .subscribe(data => {
+        this.heroes = data;
+        console.log(this.heroes);
+      });
   }
-
 }
