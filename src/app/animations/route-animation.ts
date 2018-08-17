38713ecdb4dec10a723ce49422fff89fa6e67ef1
eastflag <eastflag@gmail.com>
2018-08-17 16:53:22 +0900

    애니메이션 fixed 부분 수정

--- src/app/animations/route-animation.ts
+++ src/app/animations/route-animation.ts
@@ -45,17 +45,20 @@ export const routeAnimation =
       /* order */
       /* 1 */
       query(':enter, :leave',
-        style({ position: 'fixed', width: '100%' }),
+        style({ position: 'absolute', width: '100%' }),
         { optional: true }),
       /* 2 */
       group([  // block executes in parallel
         query(':enter', [
-          style({ transform: 'translateX(-100%)' }),
-          animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+          style({ transform: 'translateX(-100%)', opacity: 0 }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(0%)', opacity: 1 }))
+          // style({ opacity: 0 }),
+          // animate('0.5s', style({ opacity: 1 })),
         ], { optional: true }),
         query(':leave', [
           style({ transform: 'translate(0, 0)' }),
-          animate('0.5s ease-in-out', style({ transform: 'translate(0, 100%)' }))
+          animate('0.5s ease-in-out', style({ transform: 'translate(0, 100%)', opacity: 0 }))
+          // animate('0.5s', style({ opacity: 0 })),
         ], { optional: true }),
       ])
     ])
